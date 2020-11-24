124b32ee9af78827f87ff2ad6cd3fea981685699
Mustafa Aydin <mustafakilic3520@gmail.com>
2020-11-24 21:34:25 +0300

    ilkCommit_Version

---
+++ src/test/java/test1.java
@@ -0,0 +1,8 @@
+public class test1 {
+    public static void main(String[] args) {
+
+        System.out.println("githup a gönder");
+        System.out.println(".gitignore dosyasına hangi dosya ve klasörlerin gitmeyecegni belirleriz. ");
+
+    }
+}
